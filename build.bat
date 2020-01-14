ad7663d358382e2cdd02521ebf79e9c3db84d0ba
PatPL <patrykwyp@poczta.fm>
2020-01-14 14:06:53 +0100

    open build folder after building

--- build.bat
+++ build.bat
@@ -5,3 +5,4 @@ call pkg "./JS/index.js" --targets "latest-win-x64" --output "./Bin/EZ6U_win64"
 call pkg "./JS/index.js" --targets "latest-win-x86" --output "./Bin/EZ6U_win32"
 call pkg "./JS/index.js" --targets "latest-linux-x64" --output "./Bin/EZ6U_linux64"
 call pkg "./JS/index.js" --targets "latest-macos-x64" --output "./Bin/EZ6U_mac64"
+call explorer Bin
